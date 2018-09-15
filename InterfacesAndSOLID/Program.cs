f4dea9af482e2379e99215048e4312736e2b40f7
Alex Orga <alexorga.dev@gmail.com>
2018-09-15 09:35:48 -0500

    Add project files.

---
+++ InterfacesAndSOLID/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InterfacesAndSOLID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
